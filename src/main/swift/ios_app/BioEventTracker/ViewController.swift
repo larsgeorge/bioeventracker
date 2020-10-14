3938e876d68dbb0d05c2d0966dd5edb4f87ee3c0
Lars George <lars@okera.com>
2020-10-14 14:21:19 -0700

    add sources

---
+++ src/main/swift/ios_app/BioEventTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BioEventTracker
+//
+//  Created by Lars George on 12.10.20.
+//  Copyright © 2020 Lars George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
